35f7621e0a35661cd842542a2421bb9f1e96a625
Abdy Malik Mulky <abdymalikmulky@gmail.com>
2012-10-20 21:35:03 +0700

    nanininu

--- mahasiswa.php
+++ mahasiswa.php
@@ -2,7 +2,8 @@
 
 	mysql_connect('localhost','root','') or die('Cannot connect to the DB');
 	mysql_select_db('mahasiswaservice') or die('Cannot select the DB');
-
+	
+	$json = $_GET['format'];
 	if(isset($_GET['alamat']))
 	{
 		$query = mysql_query("select * from mahasiswa where alamat='$_GET[alamat]'");
@@ -14,10 +15,37 @@
 			
 		}
 		
+		if($json != '')
+		{
 		$parsing=array('mhs' => array('mahasis'=>$mahasiswa));
 		header('Content-type: application/json');
 		echo json_encode($parsing);
-		
+		}
+		else
+		{
+			header('Content-type: text/xml');
+			echo '<data>';
+			foreach($mahasiswa as $index => $post) {
+			echo '<mahasiswa>';
+				if(is_array($post)) {
+					foreach($post as $key => $value) {
+						echo '<',$key,'>';
+						echo $value;
+						/*
+						if(is_array($value)) {
+							foreach($value as $tag => $val) {
+								echo '<',$tag,'>',htmlentities($val),'</',$tag,'>';
+							}
+						}
+						*/
+						echo '</',$key,'>';
+					}
+				}
+			echo '</mahasiswa>';
+			}
+			echo '</data>';
+			
+		}
 	}
 	
 	
@@ -31,28 +59,37 @@
 			$mahasiswa[] = $maha;
 			
 		}
-		
+		if($json != '')
+		{
 		$parsing=array('mhs' => array('mahasis'=>$mahasiswa));
 		header('Content-type: application/json');
 		echo json_encode($parsing);
-		/*
-		header('Content-type: text/xml');
-		echo '<mahasiswa>';
-		foreach($mahasiswa as $index => $post) {
-			if(is_array($post)) {
-				foreach($post as $key => $value) {
-					echo '<',$key,'>';
-					if(is_array($value)) {
-						foreach($value as $tag => $val) {
-							echo '<',$tag,'>',htmlentities($val),'</',$tag,'>';
+		}
+		else
+		{
+			header('Content-type: text/xml');
+			echo '<data>';
+			echo '<mahasiswa>';
+			foreach($mahasiswa as $index => $post) {
+				if(is_array($post)) {
+					foreach($post as $key => $value) {
+						echo '<',$key,'>';
+						echo $value;
+						/*
+						if(is_array($value)) {
+							foreach($value as $tag => $val) {
+								echo '<',$tag,'>',htmlentities($val),'</',$tag,'>';
+							}
 						}
+						*/
+						echo '</',$key,'>';
 					}
-					echo '</',$key,'>';
 				}
 			}
+			echo '</mahasiswa>';
+			echo '</data>';
+			
 		}
-		echo '</mahasiswa>';
-		*/
 	}
 
 
